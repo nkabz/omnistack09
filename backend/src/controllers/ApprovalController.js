57602a425e8afa5cdf5ddff303019770020b06f5
Nikolas Abiusi <nikolasab@gmail.com>
2020-02-14 21:30:39 -0300

    made until 35:31

--- backend/src/controllers/ApprovalController.js
+++ backend/src/controllers/ApprovalController.js
@@ -1,5 +1,14 @@
+const Booking = require('../models/Booking');
+
 module.exports = {
-    async StorageEvent(req, res) {
-        return res.json({ ok: true });
+    async store(req, res) {
+        const {booking_id} = req.params;
+        const booking = await Booking.findById(booking_id).populate('spot');
+
+        booking.approved = true;
+
+        await booking.save();
+
+        return res.json(booking);
     }
 }
